345e2ee1b466f9009afb91d2e2944556d8af767d
Ilia <ilia_isakhin@epam.com>
2021-06-10 09:47:19 +0400

    c

--- src/main/kotlin/ilia/isakhin/es/async/service/EsSearchService.kt
+++ src/main/kotlin/ilia/isakhin/es/async/service/EsSearchService.kt
@@ -2,21 +2,26 @@ package ilia.isakhin.es.async.service
 
 import ilia.isakhin.es.async.client.EsSearchClient
 import ilia.isakhin.es.async.config.EsProperties
+import org.elasticsearch.action.search.SearchRequest
 import org.elasticsearch.action.search.SearchResponse
 import org.elasticsearch.client.asyncsearch.SubmitAsyncSearchRequest
 import org.elasticsearch.common.unit.Fuzziness
 import org.elasticsearch.index.query.QueryBuilders
+import org.elasticsearch.search.aggregations.AggregationBuilders
 import org.elasticsearch.search.builder.SearchSourceBuilder
 import org.springframework.stereotype.Service
 
 @Service
 class EsSearchService(private val esSearchClient: EsSearchClient, esProperties: EsProperties) {
 
-    private val indexName = esProperties.indexName
+    private val indexNames = esProperties.indexName
 
     suspend fun search(term: String): List<String> {
-        val sourceBuilder = generateSourceBuilder(term)
-        val searchRequest = SubmitAsyncSearchRequest(sourceBuilder, indexName).apply {
+        val sourceBuilder = SearchSourceBuilder().apply {
+            query(QueryBuilders.termQuery("name", term))
+            size(10_000)
+        }
+        val searchRequest = SubmitAsyncSearchRequest(sourceBuilder, indexNames).apply {
             requestCache = false
         }
         return esSearchClient.search(searchRequest)
@@ -24,10 +29,4 @@ class EsSearchService(private val esSearchClient: EsSearchClient, esProperties: 
     }
 
     private val mapResults: (t: SearchResponse) -> List<String> = { response -> response.hits.hits.map { it.id } }
-
-    private fun generateSourceBuilder(term: String) = SearchSourceBuilder()
-        .apply {
-            query(QueryBuilders.matchQuery("name", term).fuzziness(Fuzziness.ONE))
-            size(10_000)
-        }
 }
